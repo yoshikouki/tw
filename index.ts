fdd9c89d5062370daea85b6f135a20238333520b
yoshikouki <yoshikouki@gmail.com>
2021-12-13 00:00:42 +0900

    deno でつぶやく

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { Command } from "https://deno.land/x/cliffy@v0.20.1/command/mod.ts";
 import { config } from "https://deno.land/x/dotenv/mod.ts";
+import { TwitterApi } from "https://raw.githubusercontent.com/stefanuros/deno_twitter_api/master/mod.ts";
 
 const cmd = new Command()
   .name("tw")
@@ -25,9 +26,16 @@ try {
   const conf = config();
   console.log(conf.test);
 
-  const jsonResponse = await fetch("https://api.github.com/users/denoland");
-  const jsonData = await jsonResponse.json();
-  console.log(jsonData);
+  const api = new TwitterApi({
+    consumerApiKey: conf.consumerApiKey,
+    consumerApiSecret: conf.consumerApiSecret,
+    accessToken: conf.accessToken,
+    accessTokenSecret: conf.accessTokenSecret,
+  });
+  const res = await api.post("statuses/update.json", {
+    status: "deno でつぶやいてみるぞーーー",
+  });
+  console.log(await res.json());
 } catch (e) {
   console.error("[ERROR]", e);
   Deno.exit(1);
